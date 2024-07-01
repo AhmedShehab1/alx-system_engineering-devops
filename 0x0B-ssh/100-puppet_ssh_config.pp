65ccc9cbec82c75c6e50095e8a80765e5a779c0d
AhmedShehab1 <legendbtw2003@gmail.com>
2024-07-01 03:22:31 +0000

    .

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Using Puppet to configure config file in order to connect to any server without passwd authetnication
 
   $str = "HOST *\n\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school/n"
-file { '~/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
   content => $str,
 }
