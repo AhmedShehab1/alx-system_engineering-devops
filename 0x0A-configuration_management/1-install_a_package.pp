8e8f3d623a576cc1d2cd007780343d4c922dc18d
AhmedShehab1 <legendbtw2003@gmail.com>
2024-07-01 04:10:54 +0000

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Using Puppet to install Flask Where version should be 2.1.0
-exec { 'install_flask_2.1.0':
-  command => 'pip3 install Werkzeug; pip3 install flask==2.1.0',
+# install Werzeug dependency for flask
+exec {'install_werkzeug':
+  command => 'pip3 install Werkzeug',
+  path    => ['/usr/bin', 'bin'],
+  unless  => 'pip3 show Werkzeug',
+}
+# install flask v: 2.1.0
+exec {'install_flask_2.1.0':
+  command => 'pip3 install flask==2.1.0',
   path    => ['/usr/bin', '/bin'],
+  unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Exec['install_werkzeug'],
 }
