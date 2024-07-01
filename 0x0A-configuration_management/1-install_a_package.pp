38e1f1750bff8c7432e15547f2211ee39e3678af
AhmedShehab1 <legendbtw2003@gmail.com>
2024-07-01 03:49:27 +0000

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using Puppet to install Flask Where version should be 2.1.0
 exec { 'install_flask_2.1.0':
-  command => 'pip3 install flask==2.1.0 --no-deps; pip3 install Werkzeug',
+  command => 'pip3 install Werkzeug; pip3 install flask==2.1.0',
   path    => ['/usr/bin', '/bin'],
 }
