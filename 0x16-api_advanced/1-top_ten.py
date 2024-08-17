7082b590e7945734b7dc75ed009ceecfe011a5d6
AhmedShehab1 <legendbtw2003@gmail.com>
2024-08-17 02:59:29 +0000

    .

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,4 +15,4 @@ def top_ten(subreddit):
         for post in response_json.get('data', {}).get('children', []):
             print(post.get('data', {}).get('title'))
     except Exception:
-        print(None)
+        print('None')
