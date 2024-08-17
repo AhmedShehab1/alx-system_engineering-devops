25056e2b55cdf9d0c2226e1371caa5bddf37b02b
AhmedShehab1 <legendbtw2003@gmail.com>
2024-08-17 03:03:18 +0000

    .

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,9 +10,16 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(f"https://www.reddit.com/r/{subreddit}\
 /hot.json?limit=10", headers=headers)
+    if not response:
+        print('None')
+        return
     try:
         response_json = response.json()
-        for post in response_json.get('data', {}).get('children', []):
+        posts = response_json.get('data', {}).get('children', [])
+        if not posts:
+            print('None')
+            return
+        for post in posts:
             print(post.get('data', {}).get('title'))
     except Exception:
         print('None')
