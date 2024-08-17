e4f78a7bf9bb9a67efdf78900876328ad2cb62dd
AhmedShehab1 <legendbtw2003@gmail.com>
2024-08-17 03:12:51 +0000

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
-    if response.status_code == 200:
+    if response:
         response_json = response.json()
-        return response_json.get('data').get('subscribers', 0)
+        return response_json.get('data', {}).get('subscribers', 0)
+    else:
+        return 0
